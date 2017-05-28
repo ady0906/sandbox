d1e69f8ff5d56ba49aebb5d664249cc216e5102e
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-05-27 20:33:09 -0400

    cleaning up

--- helloworld.php
+++ helloworld.php
@@ -10,6 +10,7 @@
     $first = "The quick brown fox";
     $second = " jumped over the lazy dog";
 
+// testing comment
     $third = $first;
     $third .= $second;
     echo $third;
@@ -26,5 +27,15 @@
     Uppercase: <?php echo strtoupper($third); ?> <br />
     Uppercase first: <?php echo ucfirst($third); ?> <br />
     Uppercase words: <?php echo ucwords($third); ?> <br />
+    <br />
+    Length: <?php echo strlen($third) ?><br />
+    Trim: <?php echo "A" . trim(" B C D ") . "E"; ?><br />
+    Find: <?php echo strstr($third, "brown"); ?><br />
+    Replace by string: <?php echo str_replace("quick", "super-fast", $third); ?><br />
+    <br />
+    Repeat: <?php echo str_repeat($third, 2) ?><br />
+    Make susbtring: <?php echo substr($third, 5, 10); ?><br />
+    Find position: <?php echo strpos($third, "brown"); ?><br />
+    Find character: <?php echo strchr($third, "z"); ?><br />
   </body>
 </html>
